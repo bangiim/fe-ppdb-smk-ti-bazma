14265d39563ad0bf58d2614b60309fdb090aa5b2
Muhammad Ibrahim <islahboim@gmail.com>
2023-12-09 11:53:03 +0700

    change banner

--- index.php
+++ index.php
@@ -65,13 +65,13 @@
                 <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img src="img/banner-1.png" class="d-block w-100" alt="...">
+                            <img src="img/banner-1.jpg" class="d-block w-100" alt="...">
                         </div>
                         <div class="carousel-item">
-                            <img src="img/banner-2.png" class="d-block w-100" alt="...">
+                            <img src="img/banner-2.jpg" class="d-block w-100" alt="...">
                         </div>
                         <div class="carousel-item">
-                            <img src="img/banner-3.png" class="d-block w-100" alt="...">
+                            <img src="img/banner-3.jpg" class="d-block w-100" alt="...">
                         </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
